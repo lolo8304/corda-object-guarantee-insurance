2b685ba98ab0528fe9950996682ae51129f5ea4d
Lorenz Hänggi <lolo8304@gmail.com>
2019-04-14 11:44:14 +0200

    implement node based validations on 2nd node

--- workflows/src/main/java/com/template/flows/CreateObjectGuaranteeFlow.java
+++ workflows/src/main/java/com/template/flows/CreateObjectGuaranteeFlow.java
@@ -4,6 +4,8 @@ import co.paralleluniverse.fibers.Suspendable;
 import com.template.contracts.ObjectGuaranteeContract;
 import com.template.states.ObjectQuaranteeState;
 import net.corda.core.contracts.Command;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.crypto.SecureHash;
 import net.corda.core.flows.*;
 import net.corda.core.identity.Party;
 import net.corda.core.transactions.SignedTransaction;
@@ -11,6 +13,9 @@ import net.corda.core.transactions.TransactionBuilder;
 import net.corda.core.utilities.ProgressTracker;
 
 import java.time.Instant;
+import java.util.Arrays;
+
+import static net.corda.core.contracts.ContractsDSL.requireThat;
 
 public class CreateObjectGuaranteeFlow {
 
@@ -62,14 +67,21 @@ public class CreateObjectGuaranteeFlow {
                     .addOutputState(outputState, ObjectGuaranteeContract.ID)
                     .addCommand(command);
 
+            // Verifying the transaction.
+            txBuilder.verify(getServiceHub());
+
             // Signing the transaction.
             SignedTransaction signedTx = getServiceHub().signInitialTransaction(txBuilder);
 
             // Creating a session with the other party.
             FlowSession otherPartySession = initiateFlow(this.insuranceParty);
 
+            // Obtaining the counterparty's signature.
+            SignedTransaction fullySignedTx = subFlow(new CollectSignaturesFlow(
+                    signedTx, Arrays.asList(otherPartySession), CollectSignaturesFlow.tracker()));
+
             // We finalise the transaction and then send it to the counterparty.
-            subFlow(new FinalityFlow(signedTx, otherPartySession));
+            subFlow(new FinalityFlow(fullySignedTx, otherPartySession));
 
             return null;
         }
@@ -87,8 +99,32 @@ public class CreateObjectGuaranteeFlow {
         @Suspendable
         @Override
         public Void call() throws FlowException {
-            subFlow(new ReceiveFinalityFlow(otherPartySession));
+            SecureHash expectedTxId = subFlow(new SignTxFlow(otherPartySession)).getId();
+            subFlow(new ReceiveFinalityFlow(otherPartySession, expectedTxId));
             return null;
+        }
+    }
+
+    /*
+        implement a behavior on the insurance side to accept only prices of object >= 100 and <= 1000
+        this implementation can be overridden later per node
+     */
+
+    public static class SignTxFlow extends SignTransactionFlow {
+        private SignTxFlow(FlowSession otherPartySession) {
+            super(otherPartySession);
+        }
+
+        @Override
+        protected void checkTransaction(SignedTransaction stx) {
+            requireThat(require -> {
+                ContractState output = stx.getTx().getOutputs().get(0).getData();
+                require.using("This must be an guarantee transaction.", output instanceof ObjectQuaranteeState);
+                ObjectQuaranteeState object = (ObjectQuaranteeState) output;
+                require.using("The price of the object is too low. Only accepting values >= 100.", object.getPrice() >= 100);
+                require.using("The price of the object is too hight. Only accepting values <= 1000.", object.getPrice() < 1000);
+                return null;
+            });
         }
     }
 }
